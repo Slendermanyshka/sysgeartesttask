ab90a356cc443f8fe970c59092bcb7beff48c6b8
Slendermanyshka <slendermanyshka@gmail.com>
2024-02-04 11:24:19 +0200

    almost done wit h taska 3

--- js/prog_select.js
+++ js/prog_select.js
@@ -6,11 +6,72 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
     event.preventDefault();
     const data = new FormData(event.target);
-    const parsedData = parseInput(data.get("datain"));
-    const parsedCond = parseInput(data.get("condin"));
-    console.log(data);
-    console.log(parsedData);
-    console.log(parsedCond);
+    const formData = parseInput(data.get("datain"));
+    const parsedData = formData.data;
+    const parsedCond = formData.condition;
+    /*
+        {"data": [
+            {"user": "mike@mail.com", "rating": 20, "disabled": false},
+            {"user": "greg@mail.com", "rating": 14, "disabled": false},
+            {"user": "john@mail.com", "rating": 25, "disabled": true}],
+    
+         "condition": {"include": [{"disabled": true}],
+                       "sortBy": ["rating"]}
+        }
+    */
+    let modifiedData = parsedData;
+    for (const [key, value] of Object.entries(parsedCond)) {
+        modifiedData = switcher(modifiedData, key, value);
+    }
+    printOutput(modifiedData, resultarea);
+}
+function include(data, conditions) {
+    conditions.forEach(conditionObject => {
+        for (const [condition, conditionValue] of Object.entries(conditionObject)) {
+            data = data.filter(x => x[condition] == conditionValue);
+        }
+    });
+    return data;
+}
+function exclude(data, conditions) {
+    conditions.forEach(conditionObject => {
+        for (const [condition, conditionValue] of Object.entries(conditionObject)) {
+            data = data.filter(x => x[condition] != conditionValue);
+        }
+    });
+    return data;
+}
+function sort(data, conditions) {
+    let res = data;
+    conditions.forEach(key => {
+        res.sort((a, b) => {
+            return a[key] - b[key];
+        });
+    });
+    res.sort();
+    return res;
+}
+function switcher(data, condition, conditions) {
+    let res = data;
+    switch (condition) {
+        case ("include"): {
+            res = include(res, conditions);
+            break;
+        }
+        case ("exclude"): {
+            res = exclude(res, conditions);
+            break;
+        }
+        case ("sortBy"): {
+            res = sort(res, conditions);
+            break;
+        }
+        default: {
+            console.log("We do nothing");
+            break;
+        }
+    }
+    return res;
 }
 function parseInput(stringData) {
     if (stringData != "") {
--- src/prog_select.ts
+++ src/prog_select.ts
@@ -1,25 +1,94 @@
 export {};
 const form = document.getElementById("form");
 const resultarea = document.getElementById("resultarea");
-
 form!.addEventListener('submit', handleSubmit);
+
 
 
 function handleSubmit(event:SubmitEvent){
     event.preventDefault();
     const data = new FormData(event.target as HTMLFormElement);
-    const parsedData = parseInput(data.get("datain") as string);
-    const parsedCond = parseInput(data.get("condin") as string);
-    
+    const formData = parseInput(data.get("datain") as string);
+    const parsedData = formData.data as object[];
+    const parsedCond = formData.condition  as object;
+
+
+/*
+    {"data": [
+        {"user": "mike@mail.com", "rating": 20, "disabled": false},
+        {"user": "greg@mail.com", "rating": 14, "disabled": false},
+        {"user": "john@mail.com", "rating": 25, "disabled": true}],
+
+     "condition": {"include": [{"disabled": true}],
+                   "sortBy": ["rating"]}
+    }
+*/
+
+    let modifiedData = parsedData;
+
+    for(const [key, value] of Object.entries(parsedCond)) {  
+        modifiedData = switcher(modifiedData, key, value);
+    }
+    printOutput(modifiedData,resultarea as HTMLElement);
 }
 
-function sortFunc(data:object, cond:object){
-    
-    
+function include(data: any[], conditions: any[]): any[] { 
+    conditions.forEach(conditionObject => {
+        for(const [condition, conditionValue] of Object.entries(conditionObject)) {
+            data = data.filter(x => x[condition] == conditionValue);   
+        }
+    });
+
+    return data; 
 }
 
+function exclude(data: any[], conditions: any[]): any[] { 
+    conditions.forEach(conditionObject => {
+        for(const [condition, conditionValue] of Object.entries(conditionObject)) {
+            data = data.filter(x => x[condition] != conditionValue);   
+        }
+    });
 
+    return data; 
+}
 
+function sort(data: any[], conditions: any[]): any[] {
+    let res = data;
+
+    conditions.forEach(key => {
+        res.sort((a, b) => {
+            return a[key] - b[key];
+        });
+    });
+    res.sort(); 
+    
+    return res;
+}
+
+function switcher(data: any[], condition: string, conditions: any[]): any[] {
+    let res = data;
+    switch(condition){
+        case("include"):{            
+            res = include(res, conditions);
+            break; 
+        }
+
+        case("exclude"):{
+            res = exclude(res, conditions);
+            break;
+        }
+        case("sortBy"):{
+            res = sort(res, conditions);
+            break;
+        }
+        default:{
+            console.log("We do nothing");
+            break;
+        }
+    } 
+        
+    return res;
+}
 
 function parseInput(stringData: string){
     if(stringData != ""){
